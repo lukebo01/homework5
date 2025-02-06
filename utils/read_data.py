04ee5fd52b94319e6dea4dda1ab05fca6bc14755
xReniar <ilaorainer@gmail.com>
2025-02-07 00:22:38 +0100

    Add function that reads the dataframes inside ./data/raw

---
+++ utils/read_data.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import os
+
+def read_files(dir_path:str) -> None:
+    '''
+    Reads all data and return: list(filename, pandas.Dataframe)
+    '''
+    raw_dir:list[str] = sorted(os.listdir(dir_path))
+
+    dataframes = []
+
+    for folder in raw_dir:
+        folder_dir:list[str] = sorted(os.listdir(os.path.join(dir_path, folder)))
+        for data_name in folder_dir:
+            # get the filename and extension
+            split_name = data_name.split(".")
+            filename, extension = "", ""
+
+            if len(split_name) > 2:
+                filename = ".".join(split_name[:len(filename) - 1])
+                extension = split_name[-1]
+            else:
+                filename = split_name[0]
+                extension = split_name[1]
+
+            
+            file_path = os.path.join(dir_path, folder, f"{filename}.{extension}")
+            df: pd.DataFrame = None
+            if extension in ["json", "jsonl"]:
+                df = pd.read_json(file_path, lines=True)
+            
+            if extension == "csv":
+                df = pd.read_csv(file_path)
+            
+            if extension == "xls":
+                df = pd.read_excel(file_path, engine="xlrd")
+
+            df = df.drop(columns=["Unnamed: 0"], errors='ignore')
+
+            dataframes.append((filename, df))
+
+    return dataframes
