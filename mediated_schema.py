74105ebde05483de2250cc92a1aa7cfbb2e0e692
xReniar <ilaorainer@gmail.com>
2025-02-07 00:58:03 +0100

    Remove valentine and create function that returns OpenAI client

--- mediated_schema.py
+++ mediated_schema.py
@@ -1,45 +1,22 @@
 from openai import OpenAI
-from valentine.algorithms import Cupid, DistributionBased, JaccardDistanceMatcher
-from valentine import valentine_match, valentine_match_batch
 from utils.read_data import read_files
 import pandas as pd
+import json
 
-client = OpenAI(
-    base_url="",
-    api_key=""
-)
+
+keys = json.load(open("./keys.json", "r"))
+
+
+def client(model:str) -> OpenAI:
+    return OpenAI(
+        base_url="https://openrouter.ai/api/v1",
+        api_key=keys[model]["api-key"]
+    )
 
 
 def LLM():
-    pass
-
-def valentine(dir_path:str):
-    # define matcher
-    matcher = Cupid()
-
-    dfs = read_files(dir_path)
-
-    dataframes = list(map(lambda x: x[1], dfs))
-
-    #matches = valentine_match(df1, df2, matcher)
-    matches = valentine_match_batch(dataframes, dataframes, matcher)
-
-    matches_list = [
-        {
-            "Table 1 Column": match[0][1],
-            "Table 2 Column": match[1][1],
-            "Similarity Score": score
-        }
-        for match, score in matches.items()
-    ]
-
-    # Crea un DataFrame da quella lista
-    matches_df = pd.DataFrame(matches_list)
-
-    print(matches_df)
-
-
+    print(type(client("deep-seek-1.5B")))
 
 
 if __name__ == "__main__":
-    valentine("./data/raw")
+    LLM()
