bf647596e32d039e6af6d53671b1e216bda73e30
xReniar <ilaorainer@gmail.com>
2025-02-11 17:23:25 +0100

    Add script for ground_truth generation

---
+++ gt_creation.py
@@ -0,0 +1,61 @@
+import pandas as pd
+from utils.read_data import read_files
+from difflib import SequenceMatcher
+import json
+
+
+def create_raw_file():
+    final_schema = pd.read_csv("final_mediated_schema.csv", low_memory=False)
+    reduced: pd.DataFrame = final_schema[["company_name", "_source_table"]].sort_values(by="company_name")
+    reduced.to_csv("final.csv")
+
+    files: list[str, pd.DataFrame] = read_files("./data/raw")
+    for name, df in files:
+        df.to_json(f"data_json/{name}.json", orient="records", lines=True)
+
+
+def similar(a, b) -> float:
+    return SequenceMatcher(None, a, b).ratio()
+
+
+def create_initial_gt():
+    df = pd.read_csv("final.csv")
+
+    # Crea un dizionario per la mappatura
+    ground_truth = []
+
+    # Itera sulla colonna B per trovare nomi simili
+    counter = 0
+    for i, _ in enumerate(df['company_name']):
+        a:pd.Series = df.loc[i]
+        b:pd.Series = df.loc[i + 1]
+
+        a_name = str(a["company_name"])
+        b_name = str(b["company_name"])
+
+        source_a = str(a['_source_table'])
+        source_b = str(b['_source_table'])
+
+        similarity = similar(a_name,b_name)
+        if (similarity > 0.6 and similarity < 0.7) and (source_a != source_b):
+            ground_truth.append({
+                "instance": {
+                    f"{a_name}":f"{a['_source_table']}",
+                    f"{b_name}":f"{b['_source_table']}"
+                },
+                "result": None
+            })
+            counter += 1
+        if counter == 2500:
+            break
+
+    result = {}
+    result["ground_truth"] = ground_truth
+
+    with open("./data/ground_truth.json", "w") as file:
+        json.dump(result, file, indent=4)
+
+
+if __name__ == "__main__":
+    create_raw_file()
+    create_initial_gt()
